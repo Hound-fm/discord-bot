30c751233115b78513618fe5dcd8dbc06cf32220
btzr-io <btzr.io@gmail.com>
2021-03-12 17:24:52 -0600

    fix random command + minor fixes

--- src/commands/user/random.js
+++ src/commands/user/random.js
@@ -1,40 +1,68 @@
+const client = require("@/bot.js");
+const memoize = require("memoizee");
 const Hound = require("@/lib/api.js");
 const Chance = require("chance");
-const chance_group = new Chance();
-const chance_list = new Chance();
-const shuffled = [];
+const Discord = require("discord.js");
+const ErrorHandler = require("@/lib/errors");
+const EMBED = require("@/lib/embeds.js");
+const { MESSAGE_STATUS } = require("@/constants.js");
 
-/*
-const action = (args) => {
-    // Get genre
-    let [genre] = args;
-    if (genre) {
-      genre = genre.replace(/-/g, " ");
-    }
-    // Randomize content group
-    const group = chance_group.pickone(["latest", "popular"]);
-    // Get streams list
-    const list = await Hound.getStreams(genre, group);
-    // Validate list
-    if (list && list.length) {
-      const shuffled = chance_list.shuffle(list);
-      const stream = shuffled[0];
-      if (stream && stream.cannonical_url && stream.publisher_title) {
-        // client.setMessageStatus(message, MESSAGE_STATUS.READY);
-        client.inlineReply(message, { embed: EMBED.STREAM(stream) });
-      } else {
-        client.setMessageStatus(message, MESSAGE_STATUS.ERROR);
-      }
-    } else {
-      client.setMessageStatus(message, MESSAGE_STATUS.ERROR);
-    }
-}
+const chance = new Chance();
+const cacheRandomIndex = new Map();
 
-*/
+const getShuffledItems = memoize(
+  async (category, genre) => {
+    const items = await Hound.getStreams({ category, genre, size: 50 });
+    return chance.shuffle(items);
+  },
+  {
+    promise: true,
+    maxAge: 60 * 60 * 1000,
+    preFetch: true,
+    max: 50,
+  }
+);
+
+const getRandomStream = async (message, args, arg) => {
+  // Get genre
+  let stream;
+  let [genre] = args;
+  const indexKey = `index-${genre}`;
+
+  if (genre) {
+    genre = genre.replace(/-/g, " ");
+  }
+
+  // Get streams list
+  const list = await getShuffledItems("music", genre);
+
+  if (!list || !list.length) {
+    client.setMessageStatus(message, MESSAGE_STATUS.ERROR);
+    return ErrorHandler.sendError(message, ErrorHandler.ERRORS.EMPTY_SEARCH);
+  }
+
+  const randomIndex = cacheRandomIndex.get(indexKey);
+
+  if ((randomIndex || randomIndex === 0) && randomIndex < list.length) {
+    stream = list[randomIndex];
+    cacheRandomIndex.set(
+      indexKey,
+      randomIndex < list.length ? randomIndex + 1 : 0
+    );
+  } else {
+    stream = list[0];
+    cacheRandomIndex.set(indexKey, 1);
+  }
+
+  if (stream) {
+    client.setMessageStatus(message, MESSAGE_STATUS.READY);
+    client.inlineReply(message, { embed: EMBED.STREAM(stream) });
+  }
+};
 
 module.exports = {
   name: "random",
   aliases: ["shuffle"],
   description: "Get random stream",
-  execute: async () => {},
+  execute: getRandomStream,
 };
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,16 @@
 const fetch = require("node-fetch");
 const HOUND_API = "https://api.hound.fm/";
 
-const getStreams = async (genre, group) => {
-  const params = !genre ? "" : "genre=" + genre;
-  const res = await fetch(HOUND_API + `content/music?group=${group}&` + params);
+const getStreams = async ({ category = "music", genre, group = "latest" }) => {
+  const params = new URLSearchParams({ genre, group, pageSize: 100 });
+
+  if (!genre) {
+    params.delete("genre");
+  }
+
+  const res = await fetch(
+    HOUND_API + `content/${category}?` + params.toString()
+  );
   const data = await res.json();
   const list = data.data.streams;
   return list;
